67c5b287c07fca23791c3630f53e078040c392b4
mac <hackxboy1986@163.com>
2024-05-28 20:35:03 +0800

    右键菜单

--- background.js
+++ background.js
@@ -4,3 +4,30 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "id": "customMenuItem",
+    "title": "My Custom Option",
+    "contexts": ["all"]
+  });
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	  console.log('click: ',info.menuItemId);
+    if (info.menuItemId === "customMenuItem") {
+  	  navigator.clipboard.readText()
+  	   .then(text => {
+  	        console.log('剪切板内容:', text);
+  	      })
+  	   .catch(err => {
+  	        console.error('获取剪切板内容出错:', err);
+  	      });
+      //alert("Custom item clicked!");
+    }
+  });
+});
+
+// document.addEventListener("contextmenu", (e) => {
+//   e.preventDefault(); 
+//   // 在此处添加显示自定义菜单的代码
+// });
+
